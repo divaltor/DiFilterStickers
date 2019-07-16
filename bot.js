7912787448ce5149321510fb54aeb260846ebcb9
Divaltor <kuklavudu78@gmail.com@>
2019-07-16 19:57:03 +0300

    Minor optimizations
    Signed-off-by: Divaltor <kuklavudu78@gmail.com@>

--- bot.js
+++ bot.js
@@ -7,6 +7,8 @@ const {
     handleAddStickerUser
 } = require('./handlers');
 
+const { performance } = require('perf_hooks');
+
 const bot = new Telegraf(process.env.TOKEN);
 
 bot.telegram.getMe().then((botInfo) => {
@@ -16,6 +18,12 @@ bot.telegram.getMe().then((botInfo) => {
 bot.context.db = db;
 
 bot.use(handleUsers);
+bot.use(async (ctx, next) => {
+    const date = performance.now();
+    await next();
+    const res = performance.now() - date;
+    console.log(`Response time: ${res}`);
+});
 
 bot.start(handleStart);
 
