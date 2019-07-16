1d93161e351dfc8e18e2fc8e03f517addf22fe30
Divaltor <kuklavudu78@gmail.com@>
2019-07-16 19:57:03 +0300

    Minor optimizations
    Signed-off-by: Divaltor <kuklavudu78@gmail.com@>

--- middlewares/handleUsers.js
+++ middlewares/handleUsers.js
@@ -3,11 +3,18 @@ const db = require('../database');
 
 
 module.exports = async (ctx, next) => {
+    if (ctx.message === undefined) return next();
+
     const member = await telegram.getChatMember(ctx.chat.id, ctx.from.id);
     if (ctx.message.sticker && !(member.status === 'administrator' || member.status === 'creator')) {
         const user = await db.User.getData(ctx);
         if (user.stickers.includes(ctx.message.sticker.file_id)) {
-            telegram.deleteMessage(ctx.chat.id, ctx.message.message_id);
+            try {
+                telegram.deleteMessage(ctx.chat.id, ctx.message.message_id);
+            }
+            catch (e) {
+                console.log(e);
+            }
         }
     }
     return next()
